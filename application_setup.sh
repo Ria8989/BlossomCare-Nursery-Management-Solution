17d8c4f728221a5b4d0b1dca74893e5516db721f
Riya <riyapatidar29@gmail.com>
2024-05-21 19:24:57 +0530

    Updating script

--- application_setup.sh
+++ application_setup.sh
@@ -10,5 +10,8 @@ minikube start
 # Wait for all pods to be in running state
 kubectl wait --for=condition=ready pod --all -n my-app --timeout=1000s
 
+echo "$(minikube ip):30080"
+
 # Perform port forwarding
 pgrep -f "kubectl port-forward service/backend 8085:8085 -n my-app" || nohup kubectl port-forward service/backend 8085:8085 -n my-app > /dev/null 2>&1 &
+
